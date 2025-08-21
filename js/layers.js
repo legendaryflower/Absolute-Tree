08a85f458e37410f4c988373c46c1c4fad5f6e55
legendaryflower <135606026+legendaryflower@users.noreply.github.com>
2025-08-21 08:50:13 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -133,7 +133,7 @@ addLayer("b", {
     symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "#6e64c4",
